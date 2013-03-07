2b82c4404462693aebaafb3e694094620a4fca41
kekekevin <kekekevin@gmail.com>
2013-03-06 22:29:59 -0600

    add par_sheet_item validations

--- app/models/par_sheet_item.rb
+++ app/models/par_sheet_item.rb
@@ -1,5 +1,10 @@
 class ParSheetItem < ActiveRecord::Base
+
   belongs_to :par_sheet
   belongs_to :par_item
+
   attr_accessible :count, :par_count
+
+  validates :par_count, :presence => true, :numericality => { :greater_than => 0 }
+  validates :count, :presence => true, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- spec/models/par_sheet_item_spec.rb
+++ spec/models/par_sheet_item_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe ParSheetItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have a valid factory' do
+    FactoryGirl.create(:par_sheet_item).should be_valid
+  end
+
+  it 'should be invalid without a par_count' do
+    FactoryGirl.build(:par_sheet_item, par_count: nil).should_not be_valid
+  end
+
+  it 'should be invalid without a count' do
+    FactoryGirl.build(:par_sheet_item, count: nil).should_not be_valid
+  end
+
+  it 'should be invalid with a negative par_count' do
+    FactoryGirl.build(:par_sheet_item, par_count: '-1').should_not be_valid
+  end
+
+  it 'should be invalid with a zero par_count' do
+    FactoryGirl.build(:par_sheet_item, par_count: '0').should_not be_valid
+  end
+
+  it 'should be invalid with a negative count' do
+    FactoryGirl.build(:par_sheet_item, count: '-1').should_not be_valid
+  end
+
+  it 'should be valid with a zero count' do
+    FactoryGirl.build(:par_sheet_item, count: '0').should be_valid
+  end
+
 end
