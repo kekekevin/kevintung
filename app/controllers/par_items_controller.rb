981fb0add8ee6443f6d4b7c939d36db9aea4bc9e
kekekevin <kekekevin@gmail.com>
2013-03-09 10:58:51 -0600

    initial checkin for ParItems controller

--- app/controllers/par_items_controller.rb
+++ app/controllers/par_items_controller.rb
@@ -1,22 +1,36 @@
 class ParItemsController < ApplicationController
   def new
+    @par_item = ParItem.new
   end
 
   def show
+    @par_item = ParItem.find_by_id(params[:id])
   end
 
   def create
+    @par_item = ParItem.new(params[:par_item])
+    @par_item.save
+
+    redirect_to @par_item
   end
 
   def edit
+    @par_item = ParItem.find_by_id(params[:id])
   end
 
   def update
+    @par_item = ParItem.update(params[:id], params[:par_item])   
+
+    redirect_to @par_item
   end
 
   def destroy
+    ParItem.delete(params[:id])
+
+    redirect_to par_items_url
   end
 
   def index
+    @par_items = ParItem.all
   end
 end
--- spec/controllers/par_items_controller_spec.rb
+++ spec/controllers/par_items_controller_spec.rb
@@ -2,22 +2,67 @@ require 'spec_helper'
 
 describe ParItemsController do
 
-  it "should create a new item" do
+  it "should create an item" do
+    par_item = FactoryGirl.build(:par_item)
+    ParItem.should_receive(:new).and_return(par_item)
+
+    post :create, par_item: par_item.attributes
+
+    response.should redirect_to(par_item_path(par_item))
   end
 
   it "should retrieve an item to be shown" do
+    par_item = FactoryGirl.create(:par_item)
+
+    get :show, id: par_item.id
+
+    assigns(:par_item).should eq(par_item)
+    response.should render_template(:show)
+  end
+
+  it "should retrieve all items to be shown" do
+    par_item = FactoryGirl.create(:par_item)
+    
+    get :index
+
+    assigns(:par_items).should eq([par_item])
+    response.should render_template(:index)
   end
 
   it "should create a new item" do
+    ParItem.should_receive(:new)
+
+    get :new
+
+    assigns(:par_item)
+    response.should render_template(:new)
   end
 
-  it "should retrive existing item to edit" do
+  it "should retrieve existing item to edit" do
+    par_item = FactoryGirl.create(:par_item)
+
+    get :edit, id: par_item.id
+
+    assigns(:par_item).should eq(par_item)
+    response.should render_template(:edit)
   end
 
   it "should update an existing item" do
+    par_item = FactoryGirl.create(:par_item)
+    ParItem.should_receive(:update).and_return(par_item)
+
+    put :update, id: par_item.id, par_item: par_item.attributes
+
+    assigns(:par_item).should eq(par_item)
+    response.should redirect_to par_item_path(par_item)
   end
 
   it "should destroy an existing item" do
+    par_item = FactoryGirl.create(:par_item)
+
+    delete :destroy, id: par_item.id
+
+    response.should redirect_to par_items_url
   end
 
 end
