4d65365faedac1cf1e33ec32a37c1fc4bd749e1d
kekekevin <kekekevin@gmail.com>
2013-04-02 22:23:33 -0500

    add prettier remove links

--- app/models/par_sheet_item.rb
+++ app/models/par_sheet_item.rb
@@ -3,7 +3,7 @@ class ParSheetItem < ActiveRecord::Base
   belongs_to :par_sheet
   belongs_to :par_item
 
-  attr_accessible :count, :par_count
+  attr_accessible :count, :par_count, :par_item_id
 
   validates :par_count, presence: true, numericality: { greater_than: 0 }
   validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
