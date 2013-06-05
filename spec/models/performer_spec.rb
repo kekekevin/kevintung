2612ecc662e00540a921f42501e6cfce791566c6
kekekevin <kekekevin@gmail.com>
2013-06-04 20:43:44 -0500

    quick model tests

--- spec/models/performer_spec.rb
+++ spec/models/performer_spec.rb
@@ -6,4 +6,8 @@ describe Performer do
     should have_and_belong_to_many(:slots)
   end
 
+  it "should have a valid factory" do
+    FactoryGirl.create(:performer).should be_valid
+  end
+
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -6,4 +6,8 @@ describe Slot do
     should have_and_belong_to_many :performers
   end
 
+  it "should have a valid factory" do
+    FactoryGirl.create(:slot).should be_valid
+  end
+
 end
