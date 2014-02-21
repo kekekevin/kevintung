2247f598281244fe8a5792f9f520e70f1dddfdc0
kevin <kekekevin@gmail.com>
2014-02-21 21:26:29 +0000

    create and destroy tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,23 @@ class TagsController < ApplicationController
     @tag = Tag.new
   end
   
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.save
+    
+    redirect_to tags_url
+  end
+  
+  def destroy
+    Tag.find(params[:id]).destroy
+    
+    redirect_to tags_url
+  end
+  
+  private
+  
+  def tag_params
+    params.required(:tag).permit(:name)
+  end
+  
 end
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe TagsController do
 
-  describe 'new' do
+  describe "new" do
     
     it "assigns a new tag" do
       get :new
@@ -10,6 +10,44 @@ describe TagsController do
       expect(assigns :tag).to be_a_new Tag
     end
      
+  end
+  
+  describe "create" do
+    
+    it "redirects to tags" do
+      post :create, :tag => FactoryGirl.attributes_for(:tag)
+      
+      expect(response).to redirect_to(tags_url)
+    end
+    
+    it "creates a new tag" do
+      expect {
+        post :create, :tag => FactoryGirl.attributes_for(:tag)
+        }.to change(Tag, :count).by 1
+    end
+    
+  end
+  
+  describe "destroy" do
+    
+    context "with existing tag" do
+      
+      let!(:tag) { FactoryGirl.create(:tag) }
+    
+      it "redirects to tags" do
+        delete :destroy, :id => tag.id
+        
+        expect(response).to redirect_to(tags_url)
+      end
+      
+      it "deletes the tag" do
+        expect{
+          delete :destroy, :id => tag.id
+          }.to change(Tag, :count).by -1
+      end
+      
+    end
+    
   end
   
 end
