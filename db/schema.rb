b2bea9d166dea5ba4f35f086ce345d7535740efc
Kevin Tung <kekekevin@gmail.com>
2015-10-24 12:30:57 -0500

    upgrade libs

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20140207024643) do
 
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -31,14 +31,14 @@ ActiveRecord::Schema.define(version: 20140207024643) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
-  create_table "par_items", force: true do |t|
+  create_table "par_items", force: :cascade do |t|
     t.text     "description"
-    t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",        limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
-  create_table "par_sheet_items", force: true do |t|
+  create_table "par_sheet_items", force: :cascade do |t|
     t.integer  "par_count"
     t.integer  "par_sheet_id"
     t.integer  "par_item_id"
@@ -49,19 +49,19 @@ ActiveRecord::Schema.define(version: 20140207024643) do
   add_index "par_sheet_items", ["par_item_id"], name: "index_par_sheet_items_on_par_item_id"
   add_index "par_sheet_items", ["par_sheet_id"], name: "index_par_sheet_items_on_par_sheet_id"
 
-  create_table "par_sheets", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "par_sheets", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "performers", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "performers", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "performers_slots", force: true do |t|
+  create_table "performers_slots", force: :cascade do |t|
     t.integer "slot_id"
     t.integer "performer_id"
   end
@@ -69,19 +69,19 @@ ActiveRecord::Schema.define(version: 20140207024643) do
   add_index "performers_slots", ["performer_id"], name: "index_performers_slots_on_performer_id"
   add_index "performers_slots", ["slot_id"], name: "index_performers_slots_on_slot_id"
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",        limit: 255
     t.text     "body"
-    t.string   "state"
+    t.string   "state",        limit: 255
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
+    t.string   "slug",         limit: 255
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
 
-  create_table "posts_tags", id: false, force: true do |t|
+  create_table "posts_tags", id: false, force: :cascade do |t|
     t.integer "post_id", null: false
     t.integer "tag_id",  null: false
   end
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 20140207024643) do
   add_index "posts_tags", ["post_id", "tag_id"], name: "index_posts_tags_on_post_id_and_tag_id"
   add_index "posts_tags", ["tag_id", "post_id"], name: "index_posts_tags_on_tag_id_and_post_id"
 
-  create_table "prep_sheet_items", force: true do |t|
+  create_table "prep_sheet_items", force: :cascade do |t|
     t.integer  "prep_sheet_id"
     t.integer  "count"
     t.integer  "par_item_id"
@@ -102,7 +102,7 @@ ActiveRecord::Schema.define(version: 20140207024643) do
   add_index "prep_sheet_items", ["par_item_id"], name: "index_prep_sheet_items_on_par_item_id"
   add_index "prep_sheet_items", ["prep_sheet_id"], name: "index_prep_sheet_items_on_prep_sheet_id"
 
-  create_table "prep_sheets", force: true do |t|
+  create_table "prep_sheets", force: :cascade do |t|
     t.integer  "par_sheet_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
@@ -110,16 +110,16 @@ ActiveRecord::Schema.define(version: 20140207024643) do
 
   add_index "prep_sheets", ["par_sheet_id"], name: "index_prep_sheets_on_par_sheet_id"
 
-  create_table "slots", force: true do |t|
-    t.string   "name"
+  create_table "slots", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.time     "start_time"
     t.time     "end_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "tags", force: true do |t|
-    t.string   "name"
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
